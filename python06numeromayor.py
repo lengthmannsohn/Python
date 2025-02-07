5bde17748c2896a1ebaf57aef97c14ec5ede4bea
lengthmannsohn <lengthmannsohn@hotmail.com>
2025-02-07 20:25:47 +0100

    numeromayor

---
+++ python06numeromayor.py
@@ -0,0 +1,11 @@
+print("Introduzca un numero")
+numero1=int(input())
+print("Introduzca otro")
+numero2=int(input())
+if (numero1 > numero2):
+    print ("El primero es mayor que el segundo")
+elif(numero1==numero2):
+    print("Son iguales")
+else:
+    print("El segundo es mayor que el primero")   
+
