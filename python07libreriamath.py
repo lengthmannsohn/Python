80a624be64649ef8e756e377dad6c52e7d654508
lengthmannsohn <lengthmannsohn@hotmail.com>
2025-02-07 20:52:33 +0100

    libreriamath

---
+++ python07libreriamath.py
@@ -0,0 +1,15 @@
+print("Ejemplo de librerías")
+#SINTAXIS CON from
+from math import floor, ceil, trunc
+numero1 = 20
+numero2 = 3
+division = numero1 / numero2
+print("La division es ", division)
+# DECLARAMOS VARIABLES PARA ALMACENAR LOS VALORES
+varFloor = floor(division)
+varCeil = ceil(division)
+varTrunc = trunc(division)
+print("Floor ", varFloor)
+print("Ceil ", varCeil)
+print("Trunc", varTrunc)
+print("Fin del programa")
