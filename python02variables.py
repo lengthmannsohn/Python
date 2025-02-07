c7b16c9729e12d2cc3fa801f6416aedd1066b953
lengthmannsohn <lengthmannsohn@hotmail.com>
2025-02-07 18:04:01 +0100

    aab

---
+++ python02variables.py
@@ -0,0 +1,14 @@
+print("Ejemplo variables")
+#ESTO ES UN COMENTARIO
+#COMENTAR: COMMAND + K + C
+#DESCOMENTAR: COMMAND + K + U
+# las variables no se declaran con tipado
+#las variables las declaramos en minuscula
+numero = 99
+texto = "variable de tipo texto (string"
+print (numero)
+print (texto)
+
+
+#Operaciones matematicas
+
